172fca0e4808e20c2902dd8bfb79dac6f51e152a
oto.luz <multboy@hotmail.com>
2024-04-21 22:15:38 -0300

    load seed fix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,7 +14,7 @@ class ServiceProvider extends Provider
 
         // Load gateway seeds to main project
         $this->publishes([
-            __DIR__.'/database/seeds' => database_path('seeds')
+            __DIR__.'/database/seeders' => database_path('seeds')
         ]);
     }
 }
